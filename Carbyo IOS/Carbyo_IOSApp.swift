8e3512bfd9e3a7ba0372baddea4cc7ba7cd203ee
Cédric Pachis <contact@carbyo.com>
2026-01-11 14:31:48 +0100

    Initial Commit

---
+++ Carbyo IOS/Carbyo_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carbyo_IOSApp.swift
+//  Carbyo IOS
+//
+//  Created by cedric pachis on 11/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Carbyo_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
